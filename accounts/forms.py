f70dbda06961a488212f5fa21bc994b9fec5722a
Daniel Aragon <daniel.aragon@ciencias.unam.mx>
2019-10-08 16:20:40 -0500

    Fix email validation
    Require unam.mx email when signing up. Reset migrations.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
+from django import forms
+from django.core.mail import send_mail
 
 User = get_user_model()
 
@@ -12,6 +14,12 @@ class CustomAuthenticationForm(AuthenticationForm):
 
 
 class CustomUserCreationForm(PopRequestMixin, CreateUpdateAjaxMixin, UserCreationForm):
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("unam.mx"):
+            raise forms.ValidationError("The provided email does not end with 'unam.mx'")
+        return email
+
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
